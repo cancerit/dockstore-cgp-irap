c14064c1907c29f03de2edafdd2541df09b5adb6
Yaobo Xu <yx2@sanger.ac.uk>
2018-02-14 14:31:00 +0000

    fixed mulitple raw read files input bug

--- scripts/irap_wrapper.sh
+++ scripts/irap_wrapper.sh
@@ -15,56 +15,57 @@ function usage {
 
 function prepare_input_dir {
   # process arguments
-  local data_dir_name=$1; shift
-  local ref_file=$1; shift
-  local gtf=$1; shift
-  local species=$1; shift
+  local data_dir_name="$1"; shift
+  local ref_file="$1"; shift
+  local gtf="$1"; shift
+  local species="$1"; shift
   local raw_read_files=("$@")
-  local irap_ref_dir=$data_dir_name/reference/$species
-  local irap_raw_data_dir=$data_dir_name/raw_data/$species
+  local irap_ref_dir="$data_dir_name/reference/$species"
+  local irap_raw_data_dir="$data_dir_name/raw_data/$species"
   # the ref dir
-  mkdir -p $irap_ref_dir
-  ref_file_name=$(basename $ref_file)
-  ln -s $(realpath "$ref_file") "$irap_ref_dir/$ref_file_name"
+  mkdir -p "$irap_ref_dir"
+  ref_file_name="$(basename "$ref_file")"
+  ln -s "$(realpath "$ref_file")" "$irap_ref_dir/$ref_file_name"
   if tar tf "$gtf" 2> /dev/null 1>&2
   then
-    tar xzf $gtf --directory "$irap_ref_dir/"
+    tar xzf "$gtf" --directory "$irap_ref_dir/"
   else
-    gtf_file_name=$(basename $gtf)
-    ln -s $(realpath "$gtf") "$irap_ref_dir/$gtf_file_name"
+    gtf_file_name="$(basename "$gtf")"
+    ln -s "$(realpath "$gtf")" "$irap_ref_dir/$gtf_file_name"
   fi
 
   # the raw file dir
-  mkdir -p $irap_raw_data_dir
+  mkdir -p "$irap_raw_data_dir"
   for raw_read_file in "${raw_read_files[@]}"
   do
-    raw_read_file_name=$(basename $raw_read_file)
-    ln -s $(realpath "$raw_read_file") $irap_raw_data_dir/$raw_read_file_name
+    raw_read_file_name="$(basename "$raw_read_file")"
+    ln -s "$(realpath "$raw_read_file")" "$irap_raw_data_dir/$raw_read_file_name"
   done
 }
 
 function setup_contamination_db {
-  local data_dir_name=$1; shift
-  local index_dir_name=$1; shift
-  local con_genomes_index_name=$1; shift
+  local data_dir_name="$1"; shift
+  local index_dir_name="$1"; shift
+  local con_genomes_index_name="$1"; shift
   local cont_files=("$@")
 
   local con_genomes_file=$(mktemp /tmp/irap_temp_con_genomes.XXXXXX.fa)
 
   for con_g_file in "${cont_files[@]}"
   do
-    if [[ $con_g_file =~ \.gz$ ]]
+    if [[ "$con_g_file" =~ \.gz$ ]]
     then
-      gunzip -c "$con_g_file" >> $con_genomes_file
+      gunzip -c "$con_g_file" >> "$con_genomes_file"
     else
-      cat "$con_g_file" >> $con_genomes_file
+      cat "$con_g_file" >> "$con_genomes_file"
     fi
   done
-  mkdir -p $data_dir_name/contamination
-  source $IRAP_OPT/irap_setup.sh
-  $IRAP_OPT/scripts/irap_add2contaminationDB -n $con_genomes_index_name -f "$con_genomes_file" -d "$data_dir_name/$index_dir_name/"
+  mkdir -p "$data_dir_name/contamination"
+  source "$IRAP_OPT/irap_setup.sh"
+  "$IRAP_OPT/scripts/irap_add2contaminationDB" -n "$con_genomes_index_name" -f "$con_genomes_file" -d "$data_dir_name/$index_dir_name/"
 }
-e1
+
+# require at leaest 1 argument
 if [ $# -eq 0 ];
 then
   echo ""
@@ -78,18 +79,18 @@ while getopts ":hR:g:s:r:c:i:o:m:" opt; do
   case $opt in
     h ) usage
         exit 0 ;;
-    R ) REF=$OPTARG
+    R ) REF="$OPTARG"
         if [ ! -f "$OPTARG" ]; then echo -e "\nError: $OPTARG does not exist" >&2; exit 1; fi ;;
-    g ) GTF=$OPTARG
+    g ) GTF="$OPTARG"
         if [ ! -f "$OPTARG" ]; then echo -e "\nError: $OPTARG does not exist" >&2; exit 1; fi ;;
-    s ) SPECIES=$OPTARG ;;
+    s ) SPECIES="$OPTARG" ;;
     r ) if [ ! -f "$OPTARG" ]; then echo -e "\nError: $OPTARG does not exist" >&2; exit 1; fi
-        RAW_READ_FILES+=($OPTARG) ;;
-    c ) CONFIG_FILE=$OPTARG
+        RAW_READ_FILES+=("$OPTARG") ;;
+    c ) CONFIG_FILE="$OPTARG"
         if [ ! -f "$OPTARG" ]; then echo -e "\nError: $OPTARG does not exist" >&2; exit 1; fi ;;
-    i ) IRAP_PARA+=($OPTARG) ;;
-    o ) EXP_NAME=$OPTARG ;;
-    m ) CONTAMINATION_FILES+=($OPTARG) ;;
+    i ) IRAP_PARA+=("$OPTARG") ;;
+    o ) EXP_NAME="$OPTARG" ;;
+    m ) CONTAMINATION_FILES+=("$OPTARG") ;;
     \? ) echo ""
         echo "Error: Unimplemented option: -$OPTARG" >&2
         usage >&2
@@ -143,7 +144,7 @@ set -u
 if tar tf "$GTF" 2> /dev/null 1>&2
 then
   GTF_FILE=$(tar tzf "$GTF" --wildcards "*gtf" | grep -v "exon_id\.gtf$")
-  if [ $(echo "$GTF_FILE" | wc -l) != 1 ]
+  if [ "$(echo "$GTF_FILE" | wc -l)" != 1 ]
   then
     echo "find none or more reference GTF file in $GTF" >&2
     exit 1
@@ -154,7 +155,7 @@ fi
 
 # start irap
 set +u  # IRAP_PARA could be un-defined
-REF_FILE_NAME=$(basename "$REF")
+REF_FILE_NAME="$(basename "$REF")"
 irap "conf=$CONFIG_FILE" "species=$SPECIES" "reference=$REF_FILE_NAME" "gtf_file=$GTF_FILE" "name=$EXP_NAME" "data_dir=$DATA_DIR_NAME" "${IRAP_PARA[@]}"
 
 set -u
@@ -165,4 +166,5 @@ echo "gzipping fastq files"
 find "$EXP_NAME" -name '*.fastq' -print0 | xargs -0 -I {} bash -c 'echo "gzipping:" {}; gzip {}'
 
 # tar ball the whole output directory as Dosckstore can not upload whole directory to S3 for now. s3cmd-plugin version 0.0.7
-tar -zcvf "$EXP_NAME".tar.gz "$EXP_NAME" && md5sum "$EXP_NAME".tar.gz > "$EXP_NAME".tar.gz.md5
+tar -zcvf "$EXP_NAME.tar.gz" "$EXP_NAME" && md5sum "$EXP_NAME.tar.gz" > "$EXP_NAME.tar.gz.md5"
+
